74e15e2e390cdb2b68a3ef3d79dbfd2cd34d288d
Karan5663 <132004682+Karan5663@users.noreply.github.com>
2026-01-29 13:10:59 +0530

    chore: create controllers, models, and migrations structure

---
+++ Controllers/BloodController.cs
@@ -0,0 +1,45 @@
+﻿using BloodBankService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodBankService.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BloodController : Controller
+    {
+        private readonly BloodDbContext _context;
+        public BloodController(BloodDbContext context)
+        {
+            _context = context; 
+        }
+        [HttpPost]
+        public IActionResult AddDonor(Donor donor)
+        {
+            donor.CreatedAt = DateTime.UtcNow;
+            _context.Donors.Add(donor);
+            _context.SaveChanges();
+            return Ok(donor);
+        }
+
+        [HttpGet]
+        public IActionResult getAllDonors()
+        {
+            var donors = _context.Donors.ToList();
+            return Ok(donors);
+        }
+
+        [HttpGet("Count")]
+        public IActionResult GetCount()
+        {   var result=_context.Donors
+                .GroupBy(d=>d.BloodGroup)
+                .Select(g => new
+                {BloodGroup=g.Key,
+                Count=g.Count(), 
+                }).ToList();
+            return Ok(result);
+
+        }
+
+    }
+}
---
+++ Models/Donor.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BloodBankService.Models
+{
+    public class Donor
+    {
+        [Key]
+        public int DonorId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Phone { get; set; } = string.Empty;
+
+        public string BloodGroup { get; set; } = string.Empty;
+
+        public string City { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        
+    }
+    public class BloodDbContext : DbContext
+    {
+        public BloodDbContext(DbContextOptions<BloodDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Donor> Donors { get; set; }
+
+    }
+}
