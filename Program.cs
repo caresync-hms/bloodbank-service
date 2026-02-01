597787fddc80b5e57c41ab7995e2ec8f5f08765d
Prathmesh Aher <78036496+prathamaher@users.noreply.github.com>
2026-02-01 16:49:25 +0530

    Fix MySQL server version configuration

--- Program.cs
+++ Program.cs
@@ -13,8 +13,9 @@ namespace BloodBankService
             builder.Services.AddDbContext<BloodDbContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
-        ServerVersion.AutoDetect(
-            builder.Configuration.GetConnectionString("DefaultConnection")
+        new MySqlServerVersion(new Version(8, 0, 36))
+        // ServerVersion.AutoDetect(
+        //     builder.Configuration.GetConnectionString("DefaultConnection")
         )));
             builder.Services.AddCors(options =>
             {
